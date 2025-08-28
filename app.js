1355411e562244029f46c7d0c65212e564a8ff13
maroli1 <maryamsafaei944@gmail.com>
2025-08-28 02:16:46 -0700

    add files

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-
-const express = require ("express")
+const express = require("express");
 const users = require("./users");
 const validateUserId = require("./middleware");
 const app = express();
@@ -7,18 +6,12 @@ const PORT = 3000;
 app.get("/users", (req, res) => {
   res.json(users);
 });
-
 app.get("/users/:id", validateUserId, (req, res) => {
   const userId = Number(req.params.id);
   const user = users.find(u => u.id === userId);
-  if (!user) {
-    return res.status(404).json({ error: "User not found" });
-  }
+  if (!user) return res.status(404).json({ error: "User not found" });
   res.json(user);
 });
-
 app.listen(PORT, () => {
-  console.log(`server is running on http://localhost:$ {PORT}`)
-});
-
-
+  console.log(`server is running on http://localhost:${PORT}`
+  )});
