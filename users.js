dc57ef4dab1f737d7f9a327c580008aae14d6cd7
maroli1 <maryamsafaei944@gmail.com>
2025-08-31 12:30:05 -0700

    edit users

--- users.js
+++ users.js
@@ -1,6 +1,6 @@
 const users = [
     {id:1,name:"maryam",email:"maryam@gmail.com"},
-    {id:2,name:"ali",email:"ali23@gmail.com"},
+    {id:2,name:"reza",email:"reza23@gmail.com"},
     {id:3,name:"zahra",email:"zahra12@gmail.com"},
 ];
 module.exports = users;
