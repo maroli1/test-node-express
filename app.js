741b8c26ee0b760a725f17195568141046360cc9
maroli1 <maryamsafaei944@gmail.com>
2025-09-11 08:07:08 -0700

    edate name middlewere

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const users = require("./users");
-const middleware = require("./middlewares");
+const middleware = require("./middleware");
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = 3000;
